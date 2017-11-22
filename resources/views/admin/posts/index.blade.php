a2ce6ed8146a27ce4a67742d363ebc7b42056068
M511-28\admin <a9101021249@gmail.com>
2017-11-22 10:22:35 +0800

    把表單內容改為 擷取資料庫來源

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -38,15 +38,15 @@
                     </tr>
                 </thead>
                 <tbody>
-                @foreach(range(1, 20) as $id)
+                @foreach($postsas$post)
                     <tr>
-                        <td style="text-align: center">{{ $id }}</td>
-                        <td>文章標題</td>
-                        <td style="text-align: center">V</td>
+                        <td>{{$post->id}}</td>
+                        <td>{{$post->title}}</td>
+                        <td>{{($post->is_feature)?'v':'x'}}</td>
                         <td>
-                            <a href="{{ route('admin.posts.edit', $id) }}">編輯</a>
+                            <a href ="{{route('admin.posts.edit',$post->id)}}">編輯</a>
                             /
-                            <a href="#">刪除</a>
+                            <a href ="#">刪除</a>
                         </td>
                     </tr>
                 @endforeach
