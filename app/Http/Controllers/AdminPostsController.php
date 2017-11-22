86474fc3c81552619f27cf031121f8283584b5f6
M511-28\admin <a9101021249@gmail.com>
2017-11-22 10:17:03 +0800

    顯示貼文  儲存貼文  編輯貼文  更新貼文

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -3,25 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 use App\Http\Requests;
 
 class AdminPostsController extends Controller
 {
-    public function index()
-    {
-        return view('admin.posts.index');
-    }
 
-    public function create()
+public function index()
+{
+    $posts=Post::orderBy('created_at','DESC')->get();
+    $data=['posts'=>$posts];
+    return view('admin.posts.index',$data);
+}
+public function store(Request $request)
+{
+    Post::create($request->all());
+    return redirect()->route('admin.posts.index');
+}
+public function create()
     {
         return view('admin.posts.create');
     }
 
     public function edit($id)
     {
-        $data = ['id' => $id];
-
-        return view('admin.posts.edit', $data);
+        $post=Post::find($id);
+        $data=['post'=>$post];
+        return view('admin.posts.edit',$data);
     }
+public function update(Request $request,$id)
+{
+    $post=Post::find($id);
+    $post->update($request->all());
+    return redirect()->route('admin.posts.index');
+}
 }
